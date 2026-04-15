05c6959e5a70cbe5d1c5ee27bebc5f1eb8b46cca
Shivanshdgr8 <shivanshpandey2022@vitbhopal.ac.in>
2026-04-15 13:04:22 +0530

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import tsParser from '@typescript-eslint/parser';
+import n8nNodesBase from 'eslint-plugin-n8n-nodes-base';
+
+const n8nPlugin = {
+	plugins: {
+		'n8n-nodes-base': n8nNodesBase,
+	},
+	languageOptions: {
+		parser: tsParser,
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+};
+
+export default [
+	{
+		ignores: ['dist/**', 'node_modules/**'],
+	},
+	{
+		...n8nPlugin,
+		files: ['credentials/**/*.ts'],
+		rules: n8nNodesBase.configs.credentials.rules,
+	},
+	{
+		...n8nPlugin,
+		files: ['nodes/**/*.ts'],
+		rules: n8nNodesBase.configs.nodes.rules,
+	},
+];
